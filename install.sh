40aa59128fdd4726df33396083b77777064769cc
zihe.liu <zihe.liu@ele.me>
2018-03-22 15:51:09 +0800

    init project

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp pre-commit .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+echo "cp pre-commit .git/hooks/pre-commit"
---
+++ src/main/java/com/ziheliu/Main.java
@@ -0,0 +1,13 @@
+package com.ziheliu;
+
+import java.util.Random;
+
+public class Main {
+  /**
+   * 项目入口.
+   * @param args 从命令行读取的参数.
+   */
+  public static void main(String[] args) {
+    System.out.println("hello, world");
+  }
+}
